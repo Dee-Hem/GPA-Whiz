c8484a851165de1b8c11337bc42afaf0e955c66d
Olumide Adeleke <olumideemmanuel940@gmail.com>
2026-09-18 14:47:48 +0100

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.deehem.gpawhiz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 3
-    versionName = "1.1.0"
+    versionCode = 4
+    versionName = "1.1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
